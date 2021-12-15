e82b8aaea2ef0127ef52d31b38bd3428bbd04b2c
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-12-15 20:10:07 -0300

    Lessons seed, fix: lessonsDone

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -28,3 +28,21 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1,TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false );
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1,TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false );
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 01: Capítulo 01', 1, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'Material de apoio', 'https://www.youtube.com/');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 02: Capítulo 01', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, 'Material de apoio', 'https://www.youtube.com/');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 03: Capítulo 01', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, '', 'https://www.youtube.com/');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 01: Capítulo 01', 4, 1);
+
+INSERT INTO tb_task (id, descripion, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-11-25T13:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
+
+
