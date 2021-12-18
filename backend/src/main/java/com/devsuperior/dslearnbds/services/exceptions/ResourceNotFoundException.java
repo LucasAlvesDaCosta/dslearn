04f662ef7cd8b1a97dde18485ff328c70df5e25c
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-12-18 09:56:19 -0300

    Exceptions, Validation, Security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dslearnbds.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ResourceNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
